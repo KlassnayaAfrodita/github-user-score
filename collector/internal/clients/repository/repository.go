f723a131ce61f42e2520b47c39352e390e4d71d0
KlassnayaAfrodita <evgeny.kazanjan@yandex.ru>
2025-05-04 21:40:25 +0300

    добавил репозиторий в коллектор

--- collector/internal/clients/repository/repository.go
+++ collector/internal/clients/repository/repository.go
@@ -1,6 +1,11 @@
 package repository
 
-import "context"
+import (
+	"context"
+	"fmt"
+	"github.com/KlassnayaAfrodita/github-user-score/collector/pkg/database"
+	"time"
+)
 
 type User struct {
 	ID       int
@@ -27,10 +32,30 @@ func NewCollectorRepository() *CollectorRepositoryInterface {
 	return &CollectorRepository{}
 }
 
-//const SaveStatsQuery = `insert into `
+const SaveStatsQuery = `INSERT INTO user_stats (user_id, repos, stars, forks, commits, updated_at)
+		VALUES ($1, $2, $3, $4, $5, $6)
+		ON CONFLICT (user_id) DO UPDATE
+		SET repos = EXCLUDED.repos,
+		    stars = EXCLUDED.stars,
+		    forks = EXCLUDED.forks,
+		    commits = EXCLUDED.commits,
+		    updated_at = EXCLUDED.updated_at`
 
 func (repo *CollectorRepository) SaveUserStats(ctx context.Context, stats Stats) error {
-	return nil
+	tx, err := database.InitTransaction(ctx, "SaveUserStats")
+	if err != nil {
+		return fmt.Errorf("repository.SaveUserStats: %w", err)
+	}
+
+	_, err := tx.Exec(SaveStatsQuery,
+		stats.UserID,
+		stats.Repos,
+		stats.Stars,
+		stats.Forks,
+		stats.Commits,
+		time.Now(),
+	)
+
 }
 
 func (repo *CollectorRepository) CreateUser(ctx context.Context, username string) (*User, error) {
